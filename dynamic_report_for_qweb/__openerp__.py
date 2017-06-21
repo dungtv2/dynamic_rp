261fd680f09822b170a59281b06cfea41422e968
dungtv <dungtv@elephas.vn>
2017-06-21 15:56:34 +0700

    f

---
+++ dynamic_report_for_qweb/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Dynamic Template/Report(use QWeb-PDF) for Odoo",
+    'version': '1.0',
+    'category': 'Web',
+    'description': """Dynamic Template/Report(use QWeb-PDF) for Odoo""",
+    'author': 'truongdungabc.vd9@gmail.com',
+    'depends': ['web', 'report'],
+    'data': [
+        'template.xml',
+    ],
+    'images': [
+        'images/dynamic_report.png',
+    ],
+    'price': 90,
+    'currency': 'EUR',
+    'auto_install': False,
+    'web_preload': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
